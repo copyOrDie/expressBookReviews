deaf62392bbad50ce8bf4649b10e3a08b8a23412
copyOrDie <copyordie@gmail.com>
2025-03-14 15:01:08 -0400

    added register and login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,42 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+// Filter the users array for any user with the same name
+let sameNameUsers = users.filter((user)=>{
+    user.username === username;
+});
+return sameNameUsers.length == 0;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password)=>{ //returns boolean
+//Filter the users array for any user with the same user and password
+let validuser = users.filter((user) =>{
+    return (user.username === username && user.password === password);
+});
+return validuser.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if username or password is missing
+  if (!username || !password){
+    return res.status(404).json({message: "Error loggin in"});
+  }
+  // Authenticate user
+  if (authenticatedUser(username, password)){
+    //generate JWT access token
+    let accessToken = jwt.sign({data: password}, "access",{expiresIn: 60 * 60});
+    // Store access token and user name in session
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).json({message: "user successfully logged in"});
+  } else {
+    return res.status(208).json({message: "invalid Login. Check username and password"});
+  }
+ 
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,20 @@ function filterBooks(books, property, paramValue) {
 }
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  //check if values are provided
+  if (username && password){
+    if(isValid(username)){
+        // Add the user to the users array
+        users.push({username, password});
+        return res.status(200).json({message: "User successfully registered. Now you can login."});
+    } else {
+        return res.status(404).json({message: "User Already exists!"});
+    }
+  }
+  // if user or password is missing
+  return res.status(404).json({message: "Unable to register User."});
 });
 
 // Get the book list available in the shop
